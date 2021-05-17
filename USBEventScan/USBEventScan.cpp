98d02afb7c3ed82fee86862103a0762c5f7913ef
Relliko <apolywka@gmail.com>
2021-05-17 09:41:53 -0400

    cleaned up code

--- USBEventScan/USBEventScan.cpp
+++ USBEventScan/USBEventScan.cpp
@@ -70,6 +70,112 @@ int EnumQueryResults(IEnumWbemClassObject* pEnumerator,
     return n;
 }
 
+int InitializeCOM() {
+    HRESULT hres;
+
+    // Initialize COM. ------------------------------------------
+
+    hres = CoInitializeEx(0, COINIT_MULTITHREADED);
+    if (FAILED(hres)) {
+        cout << "Failed to initialize COM library. Error code = 0x"
+            << hex << hres << endl;
+        return 1;
+    }
+
+    // Set general COM security levels --------------------------
+    hres = CoInitializeSecurity(
+        NULL,
+        -1,                          // COM authentication
+        NULL,                        // Authentication services
+        NULL,                        // Reserved
+        RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
+        RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
+        NULL,                        // Authentication info
+        EOAC_NONE,                   // Additional capabilities 
+        NULL                         // Reserved
+    );
+
+
+    if (FAILED(hres)) {
+        cout << "Failed to initialize security. Error code = 0x"
+            << hex << hres << endl;
+        CoUninitialize();
+        exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
+
+IWbemLocator* InitializeWMILocator() {
+    HRESULT hres;
+    IWbemLocator* pLoc = NULL;
+
+    hres = CoCreateInstance(
+        CLSID_WbemLocator,
+        0,
+        CLSCTX_INPROC_SERVER,
+        IID_IWbemLocator, (LPVOID*)&pLoc);
+
+    if (FAILED(hres)) {
+        cout << "Failed to create IWbemLocator object."
+            << " Err code = 0x"
+            << hex << hres << endl;
+        CoUninitialize();
+        exit(EXIT_FAILURE);
+    }
+    return pLoc;
+}
+
+IWbemServices* ConnectToWMI(IWbemLocator* pLoc) {
+    HRESULT hres;
+    IWbemServices* pSvc = NULL;
+
+    hres = pLoc->ConnectServer(
+        _bstr_t(L"ROOT\\CIMV2"), // Object path of WMI namespace
+        NULL,                    // User name. NULL = current user
+        NULL,                    // User password. NULL = current
+        0,                       // Locale. NULL indicates current
+        NULL,                    // Security flags.
+        0,                       // Authority (for example, Kerberos)
+        0,                       // Context object 
+        &pSvc                    // pointer to IWbemServices proxy
+    );
+
+    if (FAILED(hres)) {
+        cout << "Could not connect. Error code = 0x"
+            << hex << hres << endl;
+        pLoc->Release();
+        CoUninitialize();
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "Connected to ROOT\\CIMV2 WMI namespace" << endl;
+
+    // Set security levels on the proxy -------------------------
+
+    hres = CoSetProxyBlanket(
+        pSvc,                        // Indicates the proxy to set
+        RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
+        RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
+        NULL,                        // Server principal name 
+        RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
+        RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
+        NULL,                        // client identity
+        EOAC_NONE                    // proxy capabilities 
+    );
+
+    if (FAILED(hres)) {
+        cout << "Could not set proxy blanket. Error code = 0x"
+            << hex << hres << endl;
+        pSvc->Release();
+        pLoc->Release();
+        CoUninitialize();
+        exit(EXIT_FAILURE);
+    }
+
+    return pSvc;
+}
+
 // If failed, exits the program
 IEnumWbemClassObject* QueryUSBDevices(IWbemServices* pSvc, IWbemLocator* pLoc) {
     HRESULT hres;
@@ -113,108 +219,21 @@ int main(int argc, char** argv) {
     HRESULT hres;
 
     // Initialize COM. ------------------------------------------
-
-    hres = CoInitializeEx(0, COINIT_MULTITHREADED);
-    if (FAILED(hres)) {
-        cout << "Failed to initialize COM library. Error code = 0x"
-            << hex << hres << endl;
-        return 1;
-    }
-
-    // Set general COM security levels --------------------------
-    hres = CoInitializeSecurity(
-        NULL,
-        -1,                          // COM authentication
-        NULL,                        // Authentication services
-        NULL,                        // Reserved
-        RPC_C_AUTHN_LEVEL_DEFAULT,   // Default authentication 
-        RPC_C_IMP_LEVEL_IMPERSONATE, // Default Impersonation  
-        NULL,                        // Authentication info
-        EOAC_NONE,                   // Additional capabilities 
-        NULL                         // Reserved
-    );
-
-
-    if (FAILED(hres)) {
-        cout << "Failed to initialize security. Error code = 0x"
-            << hex << hres << endl;
-        CoUninitialize();
-        return 1;                    // Program has failed.
-    }
-
+    InitializeCOM();
     // Obtain the initial locator to WMI -------------------------
-
-    IWbemLocator* pLoc = NULL;
-
-    hres = CoCreateInstance(
-        CLSID_WbemLocator,
-        0,
-        CLSCTX_INPROC_SERVER,
-        IID_IWbemLocator, (LPVOID*)&pLoc);
-
-    if (FAILED(hres)) {
-        cout << "Failed to create IWbemLocator object."
-            << " Err code = 0x"
-            << hex << hres << endl;
-        CoUninitialize();
-        return 1;
-    }
-
+    IWbemLocator* pLoc = InitializeWMILocator();
     // Connect to WMI through the IWbemLocator::ConnectServer method
-
-    IWbemServices* pSvc = NULL;
 
     // Connect to the root\cimv2 namespace with
     // the current user and obtain pointer pSvc
     // to make IWbemServices calls.
-    hres = pLoc->ConnectServer(
-        _bstr_t(L"ROOT\\CIMV2"), // Object path of WMI namespace
-        NULL,                    // User name. NULL = current user
-        NULL,                    // User password. NULL = current
-        0,                       // Locale. NULL indicates current
-        NULL,                    // Security flags.
-        0,                       // Authority (for example, Kerberos)
-        0,                       // Context object 
-        &pSvc                    // pointer to IWbemServices proxy
-    );
-
-    if (FAILED(hres)) {
-        cout << "Could not connect. Error code = 0x"
-            << hex << hres << endl;
-        pLoc->Release();
-        CoUninitialize();
-        return 1;                // Program has failed.
-    }
-
-    cout << "Connected to ROOT\\CIMV2 WMI namespace" << endl;
-
-
-    // Set security levels on the proxy -------------------------
-
-    hres = CoSetProxyBlanket(
-        pSvc,                        // Indicates the proxy to set
-        RPC_C_AUTHN_WINNT,           // RPC_C_AUTHN_xxx
-        RPC_C_AUTHZ_NONE,            // RPC_C_AUTHZ_xxx
-        NULL,                        // Server principal name 
-        RPC_C_AUTHN_LEVEL_CALL,      // RPC_C_AUTHN_LEVEL_xxx 
-        RPC_C_IMP_LEVEL_IMPERSONATE, // RPC_C_IMP_LEVEL_xxx
-        NULL,                        // client identity
-        EOAC_NONE                    // proxy capabilities 
-    );
-
-    if (FAILED(hres)) {
-        cout << "Could not set proxy blanket. Error code = 0x"
-            << hex << hres << endl;
-        pSvc->Release();
-        pLoc->Release();
-        CoUninitialize();
-        return 1;               // Program has failed.
-    }
+    IWbemServices* pSvc = ConnectToWMI(pLoc);
 
     short numDevices = -1;
     unsigned int totalChanges = 0; // number of additions or removals 
     unordered_map<wstring, USBDevice_t> deviceMap; // The map of devices existing on the system
 
+    // Main program loop
     while (1) {
         // Use the IWbemServices pointer to make requests of WMI ----
         IEnumWbemClassObject* pEnumerator = QueryUSBDevices(pSvc, pLoc);
@@ -244,6 +263,7 @@ int main(int argc, char** argv) {
             cout << "----------------------------------------" << endl << endl;
             cout << "Scanning USB ports..." << endl;
 
+            deviceMap = tempMap;
             numDevices = res;
         }
 
@@ -257,5 +277,4 @@ int main(int argc, char** argv) {
     CoUninitialize();
 
     return 0;   // Program successfully completed.
-
 }
